5ba80a6548cea294c6f04d643fd382d188263581
Nachetao <ignacioreyestorres2003@gmail.com>
2026-09-11 18:41:35 -0300

    db: se limpia el volumen de docker para generar migracion incial

---
+++ backend/src/migrations/1789162845992-InitialSchema.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class InitialSchema1789162845992 implements MigrationInterface {
+    name = 'InitialSchema1789162845992'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "nombre" text NOT NULL, "correo" text NOT NULL, "password" text NOT NULL, "alergias" text, CONSTRAINT "UQ_d3cf8c651c0e94ea522b61ca3ac" UNIQUE ("correo"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "foods" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "nombre" text NOT NULL, "ingredientes" text NOT NULL, "alergenosPresentes" text, "esNutricionalmenteSaludable" boolean NOT NULL, CONSTRAINT "PK_0cc83421325632f61fa27a52b59" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "foods"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+    }
+
+}
